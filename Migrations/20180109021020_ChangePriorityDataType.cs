9b85e7669531f2f60ebff289a3cc102dbbbf5124
James Liang <lzg@jimstudio.net>
2018-01-09 10:18:14 +0800

    Initial commit

---
+++ Migrations/20180109021020_ChangePriorityDataType.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MigrationRenamingException.Migrations
+{
+    public partial class ChangePriorityDataType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<ushort>(
+                name: "PriorityNew",
+                table: "Posts",
+                type: "smallint unsigned",
+                nullable: false,
+                defaultValue: 1
+            );
+
+            // copy the value from Priority to PriorityNew
+            migrationBuilder.Sql("update Posts set PriorityNew = case when Priority = 'Normal' then 1 when Priority = 'Urgent' then 2 when Priority = 'Immediate' then 3 else 1 end", true);
+
+            migrationBuilder.DropColumn("Priority", "Posts");
+
+            migrationBuilder.RenameColumn("PriorityNew", "Posts", "Priority");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MigrationRenamingException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
